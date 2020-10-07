2c8f71cfd847faa0f1d9ded141f777ebda351b5b
humi3 <bunnkichi328@gmail.com>
2020-10-07 09:03:24 +0900

    良さげな設定追加

--- wsl2/startup.sh
+++ wsl2/startup.sh
@@ -28,3 +28,4 @@ git config --global user.name "ユーザ名"
 git config --global user.email "使用するメールアドレス"
 git config --global core.quotepath false
 
+export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
